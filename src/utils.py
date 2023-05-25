95dc5fa0924004b62d384a1950536ad2f1566091
Сергей Платонов <platonovpochta@gmail.com>
2023-05-25 22:57:58 +0200

    add docstrings
    + небольшая корректировка файлов

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,22 @@ import datetime
 
 
 def read_file(file_name):
+    """
+    Читает *.json файлы.
+    :param file_name: *.json
+    :return: text file
+    """
     with open(file_name, "r", encoding="utf-8") as file:
         operations = json.load(file)
     return operations
 
 
 def last_operations(operations):
+    """
+    Сортирует операции по датам и возващает последние 5 операций.
+    :param operations: list of operations
+    :return: list of operations
+    """
     five_operations = []
     for i in operations:
         if 'date' in i and 'state' in i and 'from' in i and i['state'] == 'EXECUTED':
@@ -18,11 +28,21 @@ def last_operations(operations):
 
 
 def date_processing(date):
+    """
+    Обрабатывает формат даты
+    :param date: str, '%Y-%m-%dT%H:%M:%S.%f'
+    :return: str, '%d.%m.%Y'
+    """
     data = datetime.datetime.strptime(' '.join(date.split('T')), '%Y-%m-%d %H:%M:%S.%f')
     return data.date().strftime('%d.%m.%Y')
 
 
 def information_output(list_operations):
+    """
+    Выдаёт информацию по операциям.
+    :param list_operations: list of operations
+    :return: str, (data, description, operation_from, operation_to, amount, currency)
+    """
     for i in list_operations:
         data = date_processing(i['date'])
         description = i['description']
