8af3a7b5b3c0e9a81b37c34fa6ae8bfcfda88b5a
Сергей Платонов <platonovpochta@gmail.com>
2023-05-25 19:21:21 +0200

    add def last_operations add test_last_operations

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,12 @@ def read_file(file_name):
     with open(file_name, "r", encoding="utf-8") as file:
         operations = json.load(file)
     return operations
+
+
+def last_operations(operations):
+    five_operations = []
+    for i in operations:
+        if 'date' in i and 'state' in i and 'from' != None and i['state'] == 'EXECUTED':
+            five_operations.append(i['date'])
+    five_operations.sort()
+    return five_operations[-6:-1]
